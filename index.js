f22d0f18108b8d108dcb66287b578777ac199f43
rabiu-dev <72714053+rabiu-dev@users.noreply.github.com>
2021-09-08 15:52:49 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,102 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+/*
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+*/
 const ejs = require('ejs');
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 app.get('/', function(req, res){
   res.render('index');
 })
 
+/*
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(process.env.)
+}
+
+
+const oauth2Client = new OAuth2({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI
+})
+
+oauth2Client.setCredentials({
+  refreshToken: process.env.REFRESH_TOKEN
+})
+
+const accessToken = await new Promise((resolve, reject) => {
+  oauth2Client.getAccessToken((error, token) => {
+    if(error){
+      reject('Failed to create access token');
+    }
+    resolve(token);
+  })
+})
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: accessToken
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+})
 
 
 //Post route from contact from
-/*
-app.post('/sendmail', (req,res) => {
+app.post('/sendmail', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const message = req.body.message;
 
-    // Transport service s sud by nodemailer to send mails 
+  const mailOpts = {
+    from: name + ' &lt;' + email + '&gt;',
+    to: process.env.EMAIL,
+    subject: subject,
+    generateTextFromHTML: true,
+    html: message
+  }
+
+  transporter.sendMail(mailOpts, (error) => {
+    if(error){
+      console.log(mailOpts)
+      res.render('contact-failure')
+      throw error;
+    }
+    console.log('Email sent successfully...');
+    res.render('contact-success')
+  })
+})
+*/
+
+
+//server port
+app.listen(process.env.PORT || 4400, () => {
+  console.log('Express server started successfully on port: 4400');
+})
+
+
+/*
+ // Transport service used by nodemailer to send mails 
         here, gmail is our transport service
 
         const smtpTrans = nodemailer.createTransport({
@@ -60,9 +139,3 @@ app.post('/sendmail', (req,res) => {
           res.render('contact-failure')
         }
 })*/
-
-
-//server port
-app.listen(process.env.PORT || 4400, () => {
-  console.log('Express server started successfully on port: 4400');
-})
